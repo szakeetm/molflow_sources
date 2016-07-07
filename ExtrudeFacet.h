c4477e588e20f727313e9f675c4b289e6a70051d
Marton Ady <marton.ady@cern.ch>
2016-07-07 15:24:38 +0200

    Extrude along curve!

--- ExtrudeFacet.h
+++ ExtrudeFacet.h
@@ -53,9 +53,56 @@ private:
   GLTitledPanel	*groupBox2;
   GLLabel	*dirLabel;
   GLLabel	*baseLabel;
+  GLTitledPanel	*groupBox3;
+  GLButton	*curveGetDirButton;
+  GLButton	*curveGetBaseButton;
+  GLLabel	*label11;
+  GLLabel	*label12;
+  GLTextField	*curvedZText;
+  GLLabel	*label13;
+  GLLabel	*label14;
+  GLTextField	*curvedYText;
+  GLLabel	*label15;
+  GLLabel	*label16;
+  GLTextField	*curvedXText;
+  GLLabel	*label17;
+  GLLabel	*label23;
+  GLTextField	*curveRadiusLengthText;
+  GLToggle	*curveAgainstNormalCheckbox;
+  GLToggle	*curveTowardsNormalCheckbox;
+  GLLabel	*label18;
+  GLTextField	*curveZ0Text;
+  GLLabel	*label19;
+  GLLabel	*label20;
+  GLTextField	*curveY0Text;
+  GLLabel	*label21;
+  GLLabel	*label22;
+  GLTextField	*curveX0Text;
+  GLLabel	*label24;
+  GLTextField	*curveTotalAngleDegText;
+  GLLabel	*label25;
+  GLTextField	*curveStepsText;
+  GLLabel	*label26;
+  GLLabel	*label27;
+  GLTextField	*curveTotalAngleRadText;
+  GLLabel	*label10;
+  GLLabel	*label9;
+  GLButton	*curveFacetVButton;
+  GLButton	*curveFacetUButton;
+  GLButton	*curveFacetIndex1Button;
+  GLButton	*curveFacetCenterButton;
+  GLLabel	*label28;
+  GLLabel	*curveDirLabel;
+  GLLabel	*curveBaseLabel;
+  GLButton	*facetNYbutton;
+  GLButton	*facetNXbutton;
+  GLButton	*facetNZbutton;
 	
   int baseId, dirId;
-
+  void EnableDisableControls();
+  void ClearToggles(GLToggle* leaveChecked=NULL);
+  int AssertOneVertexSelected();
+  int AssertOneFacetSelected();
 };
 
 #endif /* _EXTRUDEFACETH_ */
