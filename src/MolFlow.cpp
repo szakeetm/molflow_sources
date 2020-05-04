c030a2b78da4581f6fffe81d24d12e860bfdcb74
Pascal Rene Baehr <pascal.baehr@cern.ch>
2020-05-05 00:41:40 +0200

    Fixed file filter bug with NFD on MacOS (from 65c0c27f of threaded repo)

--- src/MolFlow.cpp
+++ src/MolFlow.cpp
@@ -90,9 +90,9 @@ static const char *fileDesFilters = "Desorption files\0*.des\0All files\0*.*\0";
 */
 
 //NativeFileDialog compatible file filters
-std::string fileLoadFilters = "txt,xml,zip,geo,geo7z,syn,syn7z,str,stl,ase";
-std::string fileInsertFilters = "txt,xml,zip,geo,geo7z,syn,syn7z,stl";
-std::string fileSaveFilters = "zip,xml,txt,geo,geo7z,stl";
+std::string fileLoadFilters = "txt,xml,zip,geo,syn,str,stl,ase,geo7z,syn7z";
+std::string fileInsertFilters = "txt,xml,zip,geo,syn,stl,geo7z,syn7z";
+std::string fileSaveFilters = "zip,xml,txt,geo,stl,geo7z";
 std::string fileSelFilters = "sel";
 std::string fileTexFilters = "txt";
 std::string fileProfFilters = "csv;txt";
