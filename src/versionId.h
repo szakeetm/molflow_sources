ce85e84929f34c54b2b0308908f9de0c377da656
Marton Ady <marton.ady@cern.ch>
2020-09-24 15:31:47 +0200

    - Bumping src_shared for soft glprogress message update - Bumping ver to 2.8.4 beta

--- src/versionId.h
+++ src/versionId.h
@@ -5,8 +5,8 @@
 //Hard-coded identifiers, update these on new release and rebuild solution
 //---------------------------------------------------
 static const std::string appName = "Molflow";
-static const int appVersionId = 2803; //Compared with available updates. Global variable, so rebuild whole solution if changed.
-static const std::string appVersionName = "2.8.3";
+static const int appVersionId = 2804; //Compared with available updates. Global variable, so rebuild whole solution if changed.
+static const std::string appVersionName = "2.8.4 beta";
 //---------------------------------------------------
 #if defined(_DEBUG)
 static const std::string appTitle = "Molflow+ debug version (Compiled " __DATE__ " " __TIME__ ")";
