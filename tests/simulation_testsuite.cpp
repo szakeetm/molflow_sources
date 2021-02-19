8b457617a9caba4a115b9df9d482d341865f1463
pbahr <pascal.baehr@cern.ch>
2021-02-19 14:36:53 +0100

    Fixed calls to simulationManager constructor

--- tests/simulation_testsuite.cpp
+++ tests/simulation_testsuite.cpp
@@ -167,7 +167,7 @@ namespace {
         const size_t runForTSec = 20;
         std::vector<double> perfTimes;
         for(size_t runNb = 0; runNb < nRuns; ++runNb){
-            SimulationManager simManager();
+            SimulationManager simManager;
             SimulationModel model{};
             GlobalSimuState globState{};
 
@@ -289,12 +289,12 @@ namespace {
     TEST(SubProcessInit, Zero) {
 
         {
-            SimulationManager simMan();
+            SimulationManager simMan;
             EXPECT_EQ(0, simMan.InitSimUnits());
         }
 
         {
-            SimulationManager simMan();
+            SimulationManager simMan;
             simMan.useCPU = true;
             simMan.nbThreads = 0;
             simMan.InitSimUnits();
@@ -302,7 +302,7 @@ namespace {
         }
 
         {
-            SimulationManager simMan();
+            SimulationManager simMan;
             simMan.useCPU = true;
             simMan.nbThreads = 1; // more not possible,
             simMan.InitSimUnits();
@@ -312,7 +312,7 @@ namespace {
 
     TEST(SubProcessCreateAndKill, CPU) {
         {
-            SimulationManager simMan();
+            SimulationManager simMan;
             simMan.useCPU = true;
             simMan.nbThreads = 1;
             simMan.InitSimUnits();
