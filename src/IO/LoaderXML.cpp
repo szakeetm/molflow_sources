d85f8ef8059c42ba02e7eb8173427b5eebba396b
Marton Ady <marton.ady@cern.ch>
2021-11-22 10:22:40 +0100

    Proposed fix for LoaderXML heap error

--- src/IO/LoaderXML.cpp
+++ src/IO/LoaderXML.cpp
@@ -10,6 +10,7 @@
 #include "LoaderXML.h"
 #include "TimeMoments.h"
 #include "File.h"
+#include <fmt/core.h>
 
 using namespace pugi;
 using namespace FlowIO;
@@ -61,10 +62,7 @@ int LoaderXML::LoadGeometry(const std::string &inputFileName, std::shared_ptr<Si
     model->structures.resize(model->sh.nbSuper);
     for (xml_node structure : geomNode.child("Structures").children("Structure")) {
         model->structures[idx].strName = structure.attribute("name").value();
-        // For backward compatibilty with STR
-        char tmp[256];
-        sprintf(tmp, "%s.txt", model->structures[idx].strName.c_str());
-        model->structures[idx].strFileName = tmp;
+        model->structures[idx].strFileName = fmt::format("{}.txt",model->structures[idx].strName); // For backward compatibilty with STR
         idx++;
     }
 
