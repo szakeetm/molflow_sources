b86acbf3b70e106395e7e1f3352c3a52b29feb4c
Pascal Rene Baehr <pascal.baehr@cern.ch>
2022-02-25 22:24:13 +0100

    Fix: Missing M_PI_2 for MSVC, try 2

--- src/MolflowGeometry.cpp
+++ src/MolflowGeometry.cpp
@@ -17,6 +17,13 @@ GNU General Public License for more details.
 
 Full license text: https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
 */
+
+// M_PI define
+#if defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
+#define _USE_MATH_DEFINES // activate defines, e.g. M_PI_2
+#endif
+#include <cmath>
+
 #include "MolflowGeometry.h"
 #include "MolFlow.h"
 #include "Facet_shared.h"
@@ -26,7 +33,6 @@ Full license text: https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
 #include "ConvergencePlotter.h"
 #include "versionId.h"
 #include <iomanip>
-#include <cmath>
 #include <cfloat> // DBL_EPSILON
 
 #include <cereal/types/vector.hpp>
