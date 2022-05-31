3f1eff5319949a73cf5e602aa9cf3280cbc4311d
Marton Ady <marton.ady@cern.ch>
2022-05-31 15:30:56 +0200

    Bump version id to 2.8.7 beta

--- src/versionId.h
+++ src/versionId.h
@@ -6,7 +6,7 @@
 //---------------------------------------------------
 static const std::string appName = "Molflow";
 static const int appVersionId = 2806; //Compared with available updates. Global variable, so rebuild whole solution if changed.
-static const std::string appVersionName = "2.8.6";
+static const std::string appVersionName = "2.8.7 beta";
 //---------------------------------------------------
 #if defined(_DEBUG)
 static const std::string appTitle = "Molflow+ debug version (Compiled " __DATE__ " " __TIME__ ")";
