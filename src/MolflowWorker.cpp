d69682ed656c44b89e101a15064d65e41ee3229a
Marton Ady <marton.ady@cern.ch>
2022-11-21 13:45:01 +0100

    working ziplib version

--- src/MolflowWorker.cpp
+++ src/MolflowWorker.cpp
@@ -67,9 +67,9 @@ Full license text: https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
 #include "SynRad.h"
 #endif
 
-#include "ziplib/ZipArchive.h"
-#include "ziplib/ZipArchiveEntry.h"
-#include "ziplib/ZipFile.h"
+#include <ziplib/ZipArchive.h>
+#include <ziplib/ZipArchiveEntry.h>
+#include <ziplib/ZipFile.h>
 #include "File.h" //File utils (Get extension, etc)
 #include "ProcessControl.h"
 #include "versionId.h"
