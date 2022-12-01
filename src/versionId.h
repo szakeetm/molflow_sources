39ef35af6783dd4120c8661ebfef97b939fb3ba9
Marton Ady <marton.ady@cern.ch>
2022-12-01 15:35:40 +0100

    Restructured appversionid.h

--- src/versionId.h
+++ src/versionId.h
@@ -6,11 +6,14 @@
 //---------------------------------------------------
 static const std::string appName = "Molflow";
 static const int appVersionId = 2907; //Compared with available updates. Global variable, so rebuild whole solution if changed.
-static const std::string appVersionName = "2.9.7 with force counters";
-//---------------------------------------------------
+static const std::string appVersionName = "2.9.7 beta";
+
+static const std::string appTitle = "Molflow+ " + appVersionName 
 #if defined(_DEBUG)
-static const std::string appTitle = "Molflow+ " + appVersionName + " debug version (" __DATE__ " " __TIME__ ")";
-#else
-static const std::string appTitle = "Molflow+ " + appVersionName + " (" __DATE__ ")";
++ " [DEBUG mode]"
 #endif
+#if defined (MEASURE_FORCES)
++ " with force measurement"
+#endif
++ " (" __DATE__ ")";
 #endif
