89a5749e973d67e7732c717dd592669dc9d9807b
Marton Ady <marton.ady@cern.ch>
2022-12-02 16:48:04 +0100

    Remove "with forces" from app title

--- src/versionId.h
+++ src/versionId.h
@@ -8,12 +8,10 @@ static const std::string appName = "Molflow";
 static const int appVersionId = 2907; //Compared with available updates. Global variable, so rebuild whole solution if changed.
 static const std::string appVersionName = "2.9.7 beta";
 
-static const std::string appTitle = "Molflow+ " + appVersionName 
+static const std::string appTitle = "Molflow+ " + appVersionName
++ " (" __DATE__ ")"
 #if defined(_DEBUG)
-+ " [DEBUG mode]"
++" [DEBUG mode]"
 #endif
-#if defined (MEASURE_FORCES)
-+ " with force measurement"
-#endif
-+ " (" __DATE__ ")";
+;
 #endif
