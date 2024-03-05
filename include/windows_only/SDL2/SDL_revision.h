25ae62e5d82c0cbb1ace2f60d2d28db826464cc9
Marton Ady <marton.ady@cern.ch>
2024-03-05 14:21:02 +0100

    first working release build

--- include/windows_only/SDL2/SDL_revision.h
+++ include/windows_only/SDL2/SDL_revision.h
@@ -1,2 +1,8 @@
-#define SDL_REVISION ""
+/* #undef SDL_VENDOR_INFO */
 #define SDL_REVISION_NUMBER 0
+
+#ifdef SDL_VENDOR_INFO
+#define SDL_REVISION "SDL-2.28.5-no-vcs (" SDL_VENDOR_INFO ")"
+#else
+#define SDL_REVISION "SDL-2.28.5-no-vcs"
+#endif
--- include/windows_only/gsl/gsl_types.h
+++ include/windows_only/gsl/gsl_types.h
@@ -1,10 +1,10 @@
 /* gsl_types.h
  * 
- * Copyright (C) 2001 Brian Gough
+ * Copyright (C) 2001, 2007 Brian Gough
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or (at
+ * the Free Software Foundation; either version 3 of the License, or (at
  * your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful, but
@@ -22,22 +22,18 @@
 
 #ifndef GSL_VAR
 
-#ifdef WIN32
-#  ifdef GSL_DLL
+#ifdef _WIN32
+#  if 1 /*GSL_DLL*/
 #    ifdef DLL_EXPORT
 #      define GSL_VAR extern __declspec(dllexport)
-#      define GSL_EXPORT __declspec(dllexport)
 #    else
 #      define GSL_VAR extern __declspec(dllimport)
-#      define GSL_EXPORT __declspec(dllimport)
 #    endif
 #  else
 #    define GSL_VAR extern
-#    define GSL_EXPORT
 #  endif
 #else
 #  define GSL_VAR extern
-#  define GSL_EXPORT
 #endif
 
 #endif
