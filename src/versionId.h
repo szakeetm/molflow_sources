7c9a522152d0961bea609466aa368bd73c51294a
Marton Ady <marton.ady@cern.ch>
2024-05-15 16:49:23 +0900

    change appName from Molflow+ to MolFlow (affects updater event statistics)

--- src/versionId.h
+++ src/versionId.h
@@ -4,11 +4,11 @@
 #if defined(MOLFLOW)
 //Hard-coded identifiers, update these on new release
 //---------------------------------------------------
-static const std::string appName = "Molflow";
+static const std::string appName = "MolFlow";
 static const int appVersionId = 2923; //Compared with available updates 
 static const std::string appVersionName = "2.9.23 beta";
 
-static const std::string appTitle = "Molflow+ " + appVersionName
+static const std::string appTitle = "MolFlow " + appVersionName
 + " (" __DATE__ ")"
 #if defined(DEBUG)
 +" [DEBUG mode]"
